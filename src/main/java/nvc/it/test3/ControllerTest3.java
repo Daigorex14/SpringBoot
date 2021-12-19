56e3bb9bd06af685219923665b88dc010f76f200
Daigorex14 <Koonx12@gmail.com>
2021-12-19 16:09:13 +0700

    first commit

---
+++ src/main/java/nvc/it/test3/ControllerTest3.java
@@ -0,0 +1,33 @@
+package nvc.it.test3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ControllerTest3 {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String index(){
+        return "<center><h1>INDEX</h1><br><h2>Thanakit S.</h2></center>";
+    }
+
+    @RequestMapping("/name")
+    @ResponseBody
+    public String name (){
+        return "<center><h1>I am Ultra Thanakit</h1></center> <a href='/'> Go to index page</a>";
+    }
+
+    @RequestMapping("/about")
+    @ResponseBody
+    public String about(){
+        return "<center><h1>Contect</h1></center>";
+    }
+
+    @RequestMapping("/contect")
+    @ResponseBody
+    public String contect(){
+        return "<center><h1>Contect</h1></center>";
+    }
+}
