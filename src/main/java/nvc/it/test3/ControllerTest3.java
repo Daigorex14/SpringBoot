b7c0467fb0de2bfa03ed06dea28ae314ad615929
Daigorex14 <Koonx12@gmail.com>
2021-12-19 16:16:59 +0700

    fix

--- src/main/java/nvc/it/test3/ControllerTest3.java
+++ src/main/java/nvc/it/test3/ControllerTest3.java
@@ -10,24 +10,24 @@ public class ControllerTest3 {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
     public String index(){
-        return "<center><h1>INDEX</h1><br><h2>Thanakit S.</h2></center>";
+        return "<center><h1>INDEX</h1><br><h2>Thanakit S.</h2> <br> <a href='/name'> Go to Name page</a></center>";
     }
 
     @RequestMapping("/name")
     @ResponseBody
     public String name (){
-        return "<center><h1>I am Ultra Thanakit</h1></center> <a href='/'> Go to index page</a> <a href='/about'> Go to About page</a> <a href='/contect'> Go to Contect page</a>";
+        return "<center><h1>I am Ultra Thanakit</h1> <a href='/'> Go to Index page</a> <br> <a href='/about'> Go to About page</a> <br> <a href='/contect'> Go to Contect page</a> </center>";
     }
 
     @RequestMapping("/about")
     @ResponseBody
     public String about(){
-        return "<center><h1>About</h1></center>";
+        return "<center><h1>About</h1> <br> <a href='/name'> Go to Name page</a> </center>";
     }
 
     @RequestMapping("/contect")
     @ResponseBody
     public String contect(){
-        return "<center><h1>Contect</h1></center>";
+        return "<center><h1>Contect</h1> <br> <a href='/name'> Go to Name page</a></center>";
     }
 }
